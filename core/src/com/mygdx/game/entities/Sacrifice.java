8ff421691908c3094ca81bf4b6efb72f38cfc5aa
PiotrJ <me@piotrjastrzebski.io>
2016-01-30 20:33:35 +0100

    sacrifice wanders when no dangers are near

--- core/src/com/mygdx/game/entities/Sacrifice.java
+++ core/src/com/mygdx/game/entities/Sacrifice.java
@@ -1,6 +1,11 @@
 package com.mygdx.game.entities;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ai.steer.SteeringAcceleration;
+import com.badlogic.gdx.ai.steer.behaviors.BlendedSteering;
+import com.badlogic.gdx.ai.steer.behaviors.RaycastObstacleAvoidance;
+import com.badlogic.gdx.ai.steer.behaviors.Wander;
+import com.badlogic.gdx.ai.steer.utils.rays.CentralRayWithWhiskersConfiguration;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -14,7 +19,10 @@ import com.badlogic.gdx.physics.box2d.MassData;
 import com.mygdx.game.G;
 import com.mygdx.game.model.GameWorld;
 import com.mygdx.game.model.PhysicsObject;
+import com.mygdx.game.utils.BodySteerable;
+import com.mygdx.game.utils.Box2dRaycastCollisionDetector;
 import com.mygdx.game.view.WorldRenderer;
+import com.sun.org.apache.regexp.internal.RE;
 
 /**
  * Should run from player
@@ -30,6 +38,12 @@ public class Sacrifice extends Entity implements PhysicsObject {
     private float mass;
     private boolean flagForDelete = false;
     private Vector2 velocity = new Vector2();
+
+    private BodySteerable steerable;
+    private Wander<Vector2> wander;
+    private RaycastObstacleAvoidance<Vector2> avoidance;
+    private CentralRayWithWhiskersConfiguration<Vector2> rayCfg;
+    private BlendedSteering<Vector2> steering;
 
     private GameWorld gameWorld;
     private final Color color;
@@ -68,7 +82,37 @@ public class Sacrifice extends Entity implements PhysicsObject {
                 .type(BodyDef.BodyType.DynamicBody)
                 .userData(this)
                 .build();
+
         mass = body.getMass();
+        steerable = new BodySteerable();
+        steerable.setMaxLinearAcceleration(1f);
+        steerable.setMaxLinearSpeed(.75f);
+        steerable.setMaxAngularAcceleration(.5f);
+        steerable.setMaxAngularSpeed(3f);
+        steerable.setBoundingRadius(bounds.width / 2);
+        steerable.setZeroLinearSpeedThreshold(0.01f);
+        steerable.setBody(body);
+
+        wander = new Wander<Vector2>(steerable);
+        wander.setFaceEnabled(true)
+            .setAlignTolerance(.01f)
+            .setDecelerationRadius(.5f)
+            .setTimeToTarget(0.3f)
+            .setWanderOffset(2f)
+            .setWanderOrientation(MathUtils.random(360))
+            .setWanderRadius(1f)
+            .setWanderRate(MathUtils.PI2 * 2);
+
+        rayCfg = new CentralRayWithWhiskersConfiguration<Vector2>(steerable, 1f, .75f, 30f);
+
+        Box2dRaycastCollisionDetector detector = new Box2dRaycastCollisionDetector(
+            gameWorld.getBox2DWorld().getWorld());
+        avoidance = new RaycastObstacleAvoidance<Vector2>(steerable, rayCfg, detector);
+        avoidance.setDistanceFromBoundary(1f);
+        steering = new BlendedSteering<Vector2>(steerable);
+        steering.add(wander, .5f);
+        steering.add(avoidance, 1.5f);
+
         resetBox2d();
     }
 
@@ -89,16 +133,37 @@ public class Sacrifice extends Entity implements PhysicsObject {
     @Override public void drawDebug (ShapeRenderer shapeRenderer) {
 //        shapeRenderer.setColor(color);
 //        shapeRenderer.circle(position.x, position.y, bounds.width/2, 16);
+
+        shapeRenderer.end();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        Vector2 target = wander.getInternalTargetPosition();
+        Vector2 center = wander.getWanderCenter();
+        float radius = wander.getWanderRadius();
+        shapeRenderer.setColor(Color.CYAN);
+        Vector2 pos = body.getPosition();
+        shapeRenderer.line(pos.x, pos.y, center.x, center.y);
+        shapeRenderer.circle(center.x, center.y, radius, 32);
+        shapeRenderer.setColor(Color.RED);
+        shapeRenderer.circle(target.x, target.y, 0.1f, 8);
+//            wander.getInternalTargetPosition();
+//            float decelerationRadius = wander.getDecelerationRadius();
+//            shapeRenderer.setColor(Color.MAGENTA);
+//            shapeRenderer.circle(center.x, center.y, decelerationRadius, 32);
+
+        shapeRenderer.end();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
     }
 
     private Vector2 tmp = new Vector2();
     public float captureCoolDown;
     public float shootTimer;
+    public static final SteeringAcceleration<Vector2> steeringOutput = new SteeringAcceleration<Vector2>(new Vector2());
+
     @Override
     public void update(float delta) {
         position.set(body.getPosition());
         velocity.set(body.getLinearVelocity());
-        rotation = body.getAngle() * MathUtils.radDeg;
+        rotation = body.getAngle() * MathUtils.radDeg + 90;
 
         float animSpeed = MathUtils.clamp(velocity.len(), 0, 1);
         animTime += delta * animSpeed;
@@ -113,11 +178,9 @@ public class Sacrifice extends Entity implements PhysicsObject {
             // Set velocity
             velocity.set(0, 0);
             float runSpeed = 0;
-
             // Players
             for (Entity player : gameWorld.getEntityManager().getEntitiesClass(Player.class)) {
                 if (player.getPosition().dst(position) < RUN_RADIUS) {
-
                     // Direction
                     tempVec2.set(position).sub(player.getPosition()).nor();
 
@@ -133,7 +196,6 @@ public class Sacrifice extends Entity implements PhysicsObject {
             // Flames
             for (Entity flame : gameWorld.getEntityManager().getEntitiesClass(Flame.class)) {
                 if (flame.getPosition().dst(position) < RUN_RADIUS) {
-
                     // Direction
                     tempVec2.set(position).sub(flame.getPosition()).nor();
 
@@ -148,28 +210,38 @@ public class Sacrifice extends Entity implements PhysicsObject {
 
             runSpeed *= SPEED;
             velocity.nor().scl(runSpeed);
-
             if (!velocity.isZero()) {
                 body.applyLinearImpulse(velocity, body.getWorldCenter(), true);
+            } else {
+                wander.calculateSteering(steeringOutput);
+                // Update position and linear velocity.
+                if (!steeringOutput.linear.isZero()) {
+                    Gdx.app.log("", "gooo");
+                    // this method internally scales the force by deltaTime
+                    body.applyLinearImpulse(steeringOutput.linear.scl(mass), body.getWorldCenter(), true);
+                }
+                if (steeringOutput.angular != 0) {
+                    // this method internally scales the torque by deltaTime
+                    body.applyTorque(steeringOutput.angular, true);
+                }
             }
         }
-
-        Vector2 velocity = body.getLinearVelocity();
-        if (!velocity.isZero(0.001f)) {
-            float desiredAngle = velocity.angleRad();
-            float nextAngle = body.getAngle() + body.getAngularVelocity() / 60.0f;
-            float totalRotation = desiredAngle - nextAngle;
-            while (totalRotation < -180 * MathUtils.degRad)
-                totalRotation += 360 * MathUtils.degRad;
-            while (totalRotation > 180 * MathUtils.degRad)
-                totalRotation -= 360 * MathUtils.degRad;
-            float desiredAngularVelocity = totalRotation * 60;
-            float change = 90f * MathUtils.degRad; //allow 1 degree rotation per time step
-            desiredAngularVelocity = Math.min(change, Math.max(-change, desiredAngularVelocity));
-            float impulse = body.getInertia() * desiredAngularVelocity;
+//        Vector2 velocity = body.getLinearVelocity();
+//        if (!velocity.isZero(0.001f)) {
+//            float desiredAngle = velocity.angleRad();
+//            float nextAngle = body.getAngle() + body.getAngularVelocity() / 60.0f;
+//            float totalRotation = desiredAngle - nextAngle;
+//            while (totalRotation < -180 * MathUtils.degRad)
+//                totalRotation += 360 * MathUtils.degRad;
+//            while (totalRotation > 180 * MathUtils.degRad)
+//                totalRotation -= 360 * MathUtils.degRad;
+//            float desiredAngularVelocity = totalRotation * 60;
+//            float change = 90f * MathUtils.degRad; //allow 1 degree rotation per time step
+//            desiredAngularVelocity = Math.min(change, Math.max(-change, desiredAngularVelocity));
+//            float impulse = body.getInertia() * desiredAngularVelocity;
 //            Gdx.app.log("", "impulse " + impulse);
-            body.applyAngularImpulse(impulse, true);
-        }
+//            body.applyAngularImpulse(impulse, true);
+//        }
     }
 
     @Override
