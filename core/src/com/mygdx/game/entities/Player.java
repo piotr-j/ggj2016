3993e01469e01e5761db525f998c9fb61237ae64
PiotrJ <me@piotrjastrzebski.io>
2016-01-30 14:54:43 +0100

    fixed missing G

--- core/src/com/mygdx/game/entities/Player.java
+++ core/src/com/mygdx/game/entities/Player.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.mygdx.game.G;
 import com.mygdx.game.controls.PlayerController;
 import com.mygdx.game.model.GameWorld;
 import com.mygdx.game.model.PhysicsObject;
